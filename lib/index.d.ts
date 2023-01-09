573151624203d33ccaba1c69996d7da08fe8777d
Martien Dermawan Tanama <martiendt@gmail.com>
2023-01-09 10:11:30 +0700

    build 0.3

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,5 @@
 export { default as invalidPathMiddleware } from "./invalid-path-middleware.js";
 export { default as errorHandlerMiddleware } from "./error-handler-middleware.js";
-export { default as BaseError, isTrustedError } from "./base-error.js";
+export { default as BaseError, isTrustedError, IError } from "./base-error.js";
 export { default as ApiError } from "./api-error.js";
+export { find, TypeCodeStatus } from "./http-status";
